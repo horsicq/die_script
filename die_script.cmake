ce5aa70d6eda88666de08af478c201371ba4ce2c
horsicq <horsicq@gmail.com>
2021-11-07 00:42:54 +0100

    Fix: 2021-11-07

--- die_script.cmake
+++ die_script.cmake
@@ -1,13 +1,13 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
-include(${CMAKE_CURRENT_LIST_DIR}/../Formats/xformats.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../XDEX/xdex.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../XArchive/xarchive.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../Formats/xformats.cmake)
 
 set(DIE_SCRIPT_SOURCES
-    ${XFORMATS_SOURCES}
     ${XDEX_SOURCES}
     ${XARCHIVE_SOURCES}
+    ${XFORMATS_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/binary_script.cpp
     ${CMAKE_CURRENT_LIST_DIR}/die_script.cpp
     ${CMAKE_CURRENT_LIST_DIR}/die_scriptengine.cpp
