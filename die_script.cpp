9cc57e3e360c073b51357fe100890c9a15313c9f
horsicq <horsicq@gmail.com>
2021-11-07 12:21:26 +0100

    Fix: 2021-11-07

--- die_script.cpp
+++ die_script.cpp
@@ -845,9 +845,9 @@ QString DiE_Script::scanResultToTsvString(SCAN_RESULT *pScanResult)
 {
     QString sResult;
 
-    int nNumberOfRecords=pScanResult->listRecords.count();
+    qint32 nNumberOfRecords=pScanResult->listRecords.count();
 
-    for(int i=0;i<nNumberOfRecords;i++)
+    for(qint32 i=0;i<nNumberOfRecords;i++)
     {
         QString sRecord=QString("%1\t%2\t%3\t%4\t%5\n").arg(pScanResult->listRecords.at(i).sType,
                                                             pScanResult->listRecords.at(i).sName,
