945739dd1aa6ecaac97cba67d7037259a1f116fe
horsicq <horsicq@gmail.com>
2021-11-09 20:04:37 +0100

    Fix: 2021-11-09

--- die_script.cmake
+++ die_script.cmake
@@ -11,8 +11,8 @@ set(DIE_SCRIPT_SOURCES
     ${CMAKE_CURRENT_LIST_DIR}/binary_script.cpp
     ${CMAKE_CURRENT_LIST_DIR}/die_script.cpp
     ${CMAKE_CURRENT_LIST_DIR}/die_scriptengine.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/elf_script.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/mach_script.cpp
     ${CMAKE_CURRENT_LIST_DIR}/msdos_script.cpp
     ${CMAKE_CURRENT_LIST_DIR}/pe_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/mach_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/elf_script.cpp
 )
