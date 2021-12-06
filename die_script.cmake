1d9180e7acd09357cada7e10e8a0eca36cfeee2e
horsicq <horsicq@gmail.com>
2021-12-07 00:39:08 +0100

    Fix: 2021-12-07

--- die_script.cmake
+++ die_script.cmake
@@ -12,14 +12,14 @@ set(DIE_SCRIPT_SOURCES
     ${XFORMATS_SOURCES}
     ${XCAPSTONE_SOURCES}
     ${SCANITEM_SOURCES}
-    ${CMAKE_CURRENT_LIST_DIR}/global_script.cpp
     ${CMAKE_CURRENT_LIST_DIR}/binary_script.cpp
     ${CMAKE_CURRENT_LIST_DIR}/die_script.cpp
     ${CMAKE_CURRENT_LIST_DIR}/die_scriptengine.cpp
     ${CMAKE_CURRENT_LIST_DIR}/elf_script.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/global_script.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/le_script.cpp
     ${CMAKE_CURRENT_LIST_DIR}/mach_script.cpp
     ${CMAKE_CURRENT_LIST_DIR}/msdos_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/pe_script.cpp
     ${CMAKE_CURRENT_LIST_DIR}/ne_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/le_script.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/pe_script.cpp
 )
