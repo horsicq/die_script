3c7c71b1de8e0853f74eff08e85000ab08983cec
horsicq <horsicq@gmail.com>
2024-02-27 21:26:30 +0100

    Update file(s): 2024-02-27

--- die_script.cmake
+++ die_script.cmake
@@ -17,21 +17,39 @@ set(DIE_SCRIPT_SOURCES
     ${SCANITEM_SOURCES}
     ${XOPTIONS_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/binary_script.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/binary_script.h
     ${CMAKE_CURRENT_LIST_DIR}/com_script.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/com_script.h
     ${CMAKE_CURRENT_LIST_DIR}/die_script.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/die_script.h
     ${CMAKE_CURRENT_LIST_DIR}/die_scriptengine.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/die_scriptengine.h
     ${CMAKE_CURRENT_LIST_DIR}/elf_script.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/elf_script.h
     ${CMAKE_CURRENT_LIST_DIR}/global_script.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/global_script.h
     ${CMAKE_CURRENT_LIST_DIR}/le_script.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/le_script.h
     ${CMAKE_CURRENT_LIST_DIR}/lx_script.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/lx_script.h
     ${CMAKE_CURRENT_LIST_DIR}/mach_script.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/mach_script.h
     ${CMAKE_CURRENT_LIST_DIR}/msdos_script.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/msdos_script.h
     ${CMAKE_CURRENT_LIST_DIR}/ne_script.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/ne_script.h
     ${CMAKE_CURRENT_LIST_DIR}/pe_script.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/pe_script.h
     ${CMAKE_CURRENT_LIST_DIR}/xscriptengine.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xscriptengine.h
     ${CMAKE_CURRENT_LIST_DIR}/zip_script.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/zip_script.h
     ${CMAKE_CURRENT_LIST_DIR}/jar_script.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/jar_script.h
     ${CMAKE_CURRENT_LIST_DIR}/apk_script.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/apk_script.h
     ${CMAKE_CURRENT_LIST_DIR}/ipa_script.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/ipa_script.h
     ${CMAKE_CURRENT_LIST_DIR}/dex_script.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dex_script.h
 )
