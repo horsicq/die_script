ce05c37309318959667b75a0cc8de18d288be444
horsicq <horsicq@gmail.com>
2025-02-14 08:09:31 +0100

    Update file(s): 2025-02-14

--- binary_script.cpp
+++ binary_script.cpp
@@ -90,7 +90,7 @@ Binary_Script::Binary_Script(XBinary *pBinary, XBinary::FILEPART filePart, OPTIO
     g_bIsFmtChecking = false;
     g_bIsFmtCheckingDeep = false;
 
-    g_disasmCore.setMode(XBinary::getDisasmMode(&g_memoryMap), XBinary::SYNTAX_DEFAULT);
+    g_disasmCore.setMode(XBinary::getDisasmMode(&g_memoryMap));
 }
 
 Binary_Script::~Binary_Script()
