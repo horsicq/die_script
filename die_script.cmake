8e4a652344b9313bd812c310668bd619bfacdc31
horsicq <horsicq@gmail.com>
2025-07-22 19:39:53 +0200

    Update file(s): 2025-07-22

--- die_script.cmake
+++ die_script.cmake
@@ -14,66 +14,14 @@ endif()
 
 set(DIE_SCRIPT_SOURCES
     ${DIE_SCRIPT_SOURCES}
-    ${CMAKE_CURRENT_LIST_DIR}/amiga_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/amiga_script.h
-    ${CMAKE_CURRENT_LIST_DIR}/archive_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/archive_script.h
-    ${CMAKE_CURRENT_LIST_DIR}/binary_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/binary_script.h
-    ${CMAKE_CURRENT_LIST_DIR}/com_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/com_script.h
     ${CMAKE_CURRENT_LIST_DIR}/die_script.cpp
     ${CMAKE_CURRENT_LIST_DIR}/die_script.h
     ${CMAKE_CURRENT_LIST_DIR}/die_scriptengine.cpp
     ${CMAKE_CURRENT_LIST_DIR}/die_scriptengine.h
-    ${CMAKE_CURRENT_LIST_DIR}/dos16m_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/dos16m_script.h
-    ${CMAKE_CURRENT_LIST_DIR}/dos4g_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/dos4g_script.h
-    ${CMAKE_CURRENT_LIST_DIR}/elf_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/elf_script.h
     ${CMAKE_CURRENT_LIST_DIR}/global_script.cpp
     ${CMAKE_CURRENT_LIST_DIR}/global_script.h
-    ${CMAKE_CURRENT_LIST_DIR}/le_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/le_script.h
-    ${CMAKE_CURRENT_LIST_DIR}/lx_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/lx_script.h
-    ${CMAKE_CURRENT_LIST_DIR}/mach_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/mach_script.h
-    ${CMAKE_CURRENT_LIST_DIR}/msdos_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/msdos_script.h
-    ${CMAKE_CURRENT_LIST_DIR}/ne_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/ne_script.h
-    ${CMAKE_CURRENT_LIST_DIR}/pe_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/pe_script.h
     ${CMAKE_CURRENT_LIST_DIR}/xscriptengine.cpp
     ${CMAKE_CURRENT_LIST_DIR}/xscriptengine.h
-    ${CMAKE_CURRENT_LIST_DIR}/zip_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/zip_script.h
-    ${CMAKE_CURRENT_LIST_DIR}/jar_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/jar_script.h
-    ${CMAKE_CURRENT_LIST_DIR}/apk_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/apk_script.h
-    ${CMAKE_CURRENT_LIST_DIR}/ipa_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/ipa_script.h
-    ${CMAKE_CURRENT_LIST_DIR}/dex_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/dex_script.h
-    ${CMAKE_CURRENT_LIST_DIR}/npm_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/npm_script.h
-    ${CMAKE_CURRENT_LIST_DIR}/rar_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/rar_script.h
-    ${CMAKE_CURRENT_LIST_DIR}/machofat_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/machofat_script.h
     ${CMAKE_CURRENT_LIST_DIR}/util_script.cpp
     ${CMAKE_CURRENT_LIST_DIR}/util_script.h
-    ${CMAKE_CURRENT_LIST_DIR}/javaclass_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/javaclass_script.h
-    ${CMAKE_CURRENT_LIST_DIR}/pdf_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/pdf_script.h
-    ${CMAKE_CURRENT_LIST_DIR}/cfbf_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/cfbf_script.h
-    ${CMAKE_CURRENT_LIST_DIR}/jpeg_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/jpeg_script.h
-    ${CMAKE_CURRENT_LIST_DIR}/image_script.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/image_script.h
 )
--- die_script.pri
+++ die_script.pri
@@ -7,70 +7,18 @@ INCLUDEPATH += $$PWD
 DEPENDPATH += $$PWD
 
 HEADERS += \
-    $$PWD/amiga_script.h \
-    $$PWD/apk_script.h \
-    $$PWD/archive_script.h \
-    $$PWD/binary_script.h \
-    $$PWD/cfbf_script.h \
-    $$PWD/com_script.h \
-    $$PWD/dex_script.h \
     $$PWD/die_script.h \
     $$PWD/die_scriptengine.h \
-    $$PWD/dos16m_script.h \
-    $$PWD/dos4g_script.h \
-    $$PWD/elf_script.h \
     $$PWD/global_script.h \
-    $$PWD/image_script.h \
-    $$PWD/ipa_script.h \
-    $$PWD/javaclass_script.h \
-    $$PWD/jar_script.h \
-    $$PWD/jpeg_script.h \
-    $$PWD/le_script.h \
-    $$PWD/lx_script.h \
-    $$PWD/mach_script.h \
-    $$PWD/msdos_script.h \
-    $$PWD/ne_script.h \
-    $$PWD/pe_script.h \
-    $$PWD/pdf_script.h \
-    $$PWD/npm_script.h \
-    $$PWD/rar_script.h \
-    $$PWD/machofat_script.h \
     $$PWD/util_script.h \
-    $$PWD/xscriptengine.h \
-    $$PWD/zip_script.h
+    $$PWD/xscriptengine.h 
 
 SOURCES += \
-    $$PWD/amiga_script.cpp \
-    $$PWD/apk_script.cpp \
-    $$PWD/archive_script.cpp \
-    $$PWD/binary_script.cpp \
-    $$PWD/cfbf_script.cpp \
-    $$PWD/com_script.cpp \
-    $$PWD/dex_script.cpp \
     $$PWD/die_script.cpp \
     $$PWD/die_scriptengine.cpp \
-    $$PWD/dos16m_script.cpp \
-    $$PWD/dos4g_script.cpp \
-    $$PWD/elf_script.cpp \
     $$PWD/global_script.cpp \
-    $$PWD/image_script.cpp \
-    $$PWD/ipa_script.cpp \
-    $$PWD/javaclass_script.cpp \
-    $$PWD/jar_script.cpp \
-    $$PWD/jpeg_script.cpp \
-    $$PWD/le_script.cpp \
-    $$PWD/lx_script.cpp \
-    $$PWD/mach_script.cpp \
-    $$PWD/msdos_script.cpp \
-    $$PWD/ne_script.cpp \
-    $$PWD/pe_script.cpp \
-    $$PWD/pdf_script.cpp \
-    $$PWD/npm_script.cpp \
-    $$PWD/rar_script.cpp \
-    $$PWD/machofat_script.cpp \
     $$PWD/util_script.cpp \
-    $$PWD/xscriptengine.cpp \
-    $$PWD/zip_script.cpp
+    $$PWD/xscriptengine.cpp
 
 !contains(XCONFIG, xscanengine) {
     XCONFIG += xscanengine
