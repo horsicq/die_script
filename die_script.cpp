2daedb38313114497b4515ebddc767d91899397b
horsicq <horsicq@gmail.com>
2025-10-07 00:32:26 +0200

    Update file(s): 2025-10-07

--- die_script.cpp
+++ die_script.cpp
@@ -204,6 +204,7 @@ void DiE_Script::processDetect(SCANID *pScanID, XScanEngine::SCAN_RESULT *pScanR
     _options.bIsDeepScan = pScanOptions->bIsDeepScan;
     _options.bIsHeuristicScan = pScanOptions->bIsHeuristicScan;
     _options.bIsAggressiveScan = pScanOptions->bIsAggressiveScan;
+    _options.bIsRecursiveScan = pScanOptions->bIsRecursiveScan;
     _options.bIsVerbose = pScanOptions->bIsVerbose;
     _options.bIsProfiling = pScanOptions->bLogProfiling;
 
